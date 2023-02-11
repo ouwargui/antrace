2f5cfd73a59299519c1b3b00f35b1c25ecf83939
ouwargui <guikemon2@gmail.com>
2023-02-11 16:57:05 -0300

    feat: create style guide and add some packages

---
+++ src/styles/index.ts
@@ -0,0 +1,11 @@
+export enum Font {
+  REGULAR = 'Rubik_400Regular',
+  MEDIUM = 'Rubik_500Medium',
+  SEMIBOLD = 'Rubik_600SemiBold',
+  BOLD = 'Rubik_700Bold',
+}
+
+export enum Colors {
+  BACKGROUND = '#232323',
+  WHITE = '#FFFFFF',
+}
